aaf139f6e6d0d7e3d79efecc04973ed80327e96e
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-23 22:54:55 +0300

    update

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,21 +1,21 @@
 import React, { useContext } from "react";
 import VideoCard from "../components/VideoCard.jsx";
 import { AppContext } from "../context/AppContext.jsx";
-import Pagination from "react-bootstrap/Pagination";
+// import Pagination from "react-bootstrap/Pagination";
 
 const Video = () => {
   const { videoData1, setVideoData } = useContext(AppContext);
   console.log(videoData1);
 
-  let active = 1;
-  let items = [];
-  for (let number = 1; number <= 5; number++) {
-    items.push(
-      <Pagination.Item key={number} active={number === active}>
-        {number}
-      </Pagination.Item>
-    );
-  }
+  // let active = 1;
+  // let items = [];
+  // for (let number = 1; number <= 5; number++) {
+  //   items.push(
+  //     <Pagination.Item key={number} active={number === active}>
+  //       {number}
+  //     </Pagination.Item>
+  //   );
+  // }
   return (
     <div>
       <div className="d-flex flex-wrap gap-3 justify-content-center mt-5">
@@ -27,9 +27,9 @@ const Video = () => {
           />
         ))}
       </div>
-      <div>
+      {/* <div>
         <Pagination>{items}</Pagination>
-      </div>
+      </div> */}
     </div>
   );
 };
